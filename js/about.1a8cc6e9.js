8b094d8c466a39a5792c90647f1b04a9d502cf72
Facundo <facundo.paez@davinci.edu.ar>
2025-04-29 16:48:32 -0300

    Updates

---
+++ js/about.1a8cc6e9.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkparcial_2"]=self["webpackChunkparcial_2"]||[]).push([[443],{6493:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var a=function(){var n=this;n._self._c;return n._m(0)},s=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],l=u(1001),r={},e=(0,l.Z)(r,a,s,!1,null,null,null),i=e.exports}}]);
+//# sourceMappingURL=about.1a8cc6e9.js.map
